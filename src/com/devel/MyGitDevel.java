fd4c794cc9ba0686387d42128e1ebcb0ccfeca28
Pgoud <praveen.goud@hotmail.com>
2018-06-12 17:34:57 +0530

    First commit to Devel branch.

---
+++ src/com/devel/MyGitDevel.java
@@ -0,0 +1,12 @@
+package com.devel;
+
+public class MyGitDevel {
+
+   public final static void main(String[] args) {
+	
+	System.out.println("My Git Devel ! Praveen -- Standalone");
+//System.out.println("My Git Devel! Modified Praveen -- Standalone");
+
+   }
+
+}
